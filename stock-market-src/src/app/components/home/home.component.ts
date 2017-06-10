52a203af8ff9ebc8a77688c7fedaa7ca8e2232b6
Andy Lindsay <andydlindsay@gmail.com>
2017-06-09 17:27:13 -0700

    adds angular material

--- stock-market-src/src/app/components/home/home.component.ts
+++ stock-market-src/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SocketService } from '../../services/socket.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -11,14 +13,26 @@ export class HomeComponent implements OnInit, OnDestroy {
   stocks = [];
   connection;
   stock;
+  stockForm: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     private socketService: SocketService
   ) { }
 
   sendStock() {
-    this.socketService.sendStock(this.stock);
-    this.stock = '';
+    // check if form is valid
+    if (this.stockForm.valid) {
+
+      // check if stock symbol is valid
+
+      this.stock = this.stockForm.value.stock.toUpperCase();
+
+
+      this.socketService.sendStock(this.stock);
+      this.stockForm.reset();
+      this.stock = '';
+    }
   }
 
   ngOnInit() {
@@ -27,12 +41,50 @@ export class HomeComponent implements OnInit, OnDestroy {
         this.stocks.push(stock);
       }
     );
+    this.buildForm();
   }
 
   ngOnDestroy() {
     this.connection.unsubscribe();
   }
 
-  
+  buildForm() {
+    this.stockForm = this.fb.group({
+      'stock': ['', [
+        Validators.required
+      ]]
+    });
+    this.stockForm.valueChanges.subscribe(data => this.onValueChanged(data));
+  }  
+
+  // onValueChanged function taken from the Angular Cookbook's Form Validation section
+  // https://angular.io/docs/ts/latest/cookbook/form-validation.html
+  onValueChanged(data?: any) {
+    if (!this.stockForm) { return; }
+    const form = this.stockForm;
+
+    for (const field in this.formErrors) {
+      // clear previous error message if any
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  formErrors = {
+    'stock': ''
+  }
+
+  validationMessages = {
+    'stock': {
+      'required': 'A stock is required.'
+    }
+  }
 
 }
